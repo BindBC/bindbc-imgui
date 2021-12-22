8d67645ce28ae019359a27e3496e14150ea77a41
Joshua T. Fisher <playmer@gmail.com>
2021-12-22 06:08:40 -0800

    Update build script to use VS2022, rerun build.
    - Fixes #10, looked at the resulting binaries in Dependencies and they seem to point to the  correct DLLs now.

--- BuildDependencies.bat
+++ BuildDependencies.bat
@@ -1,11 +1,11 @@
 cd deps
 mkdir build_x64
 cd build_x64
-cmake -G "Visual Studio 16 2019" -Ax64 ..
+cmake -G "Visual Studio 17 2022" -Ax64 ..
 cmake --build . --config Release
 cd ..
 mkdir build_x86
 cd build_x86
-cmake -G "Visual Studio 16 2019" -AWin32 ..
+cmake -G "Visual Studio 17 2022" -AWin32 ..
 cmake --build . --config Release
 cd ../../
