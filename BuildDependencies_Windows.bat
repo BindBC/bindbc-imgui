1e4b622050accfb66faf75a823c72b02fa86b188
Joshua T. Fisher <playmer@gmail.com>
2022-06-02 00:22:03 -0700

    Renamed windows build folders in line with Linux folders

--- BuildDependencies_Windows.bat
+++ BuildDependencies_Windows.bat
@@ -1,61 +1,61 @@
 cd deps
-mkdir build_x64_cimguiStatic_StaticCRT
-cd build_x64_cimguiStatic_StaticCRT
+mkdir build_windows_x64_cimguiStatic_StaticCRT
+cd build_windows_x64_cimguiStatic_StaticCRT
 cmake -G "Visual Studio 17 2022" -Ax64 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT= ..
 cmake --build . --config Release
 cd ..
-mkdir build_x64_cimguiStatic_DynamicCRT
-cd build_x64_cimguiStatic_DynamicCRT
+mkdir build_windows_x64_cimguiStatic_DynamicCRT
+cd build_windows_x64_cimguiStatic_DynamicCRT
 cmake -G "Visual Studio 17 2022" -Ax64 -DSTATIC_CIMGUI= ..
 cmake --build . --config Release
 cd ..
-mkdir build_x64_cimguiDynamic_StaticCRT
-cd build_x64_cimguiDynamic_StaticCRT
+mkdir build_windows_x64_cimguiDynamic_StaticCRT
+cd build_windows_x64_cimguiDynamic_StaticCRT
 cmake -G "Visual Studio 17 2022" -Ax64 -DWINDOWS_STATIC_CRT= ..
 cmake --build . --config Release
 cd ..
-mkdir build_x64_cimguiDynamic_DynamicCRT
-cd build_x64_cimguiDynamic_DynamicCRT
+mkdir build_windows_x64_cimguiDynamic_DynamicCRT
+cd build_windows_x64_cimguiDynamic_DynamicCRT
 cmake -G "Visual Studio 17 2022" -Ax64 ..
 cmake --build . --config Release
 cd ..
-mkdir build_x86_cimguiStatic_StaticCRT
-cd build_x86_cimguiStatic_StaticCRT
+mkdir build_windows_x86_cimguiStatic_StaticCRT
+cd build_windows_x86_cimguiStatic_StaticCRT
 cmake -G "Visual Studio 17 2022" -AWin32 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT= ..
 cmake --build . --config Release
 cd ..
-mkdir build_x86_cimguiStatic_DynamicCRT
-cd build_x86_cimguiStatic_DynamicCRT
+mkdir build_windows_x86_cimguiStatic_DynamicCRT
+cd build_windows_x86_cimguiStatic_DynamicCRT
 cmake -G "Visual Studio 17 2022" -AWin32 -DSTATIC_CIMGUI= ..
 cmake --build . --config Release
 cd ..
-mkdir build_x86_cimguiDynamic_StaticCRT
-cd build_x86_cimguiDynamic_StaticCRT
+mkdir build_windows_x86_cimguiDynamic_StaticCRT
+cd build_windows_x86_cimguiDynamic_StaticCRT
 cmake -G "Visual Studio 17 2022" -AWin32 -DWINDOWS_STATIC_CRT= ..
 cmake --build . --config Release
 cd ..
-mkdir build_x86_cimguiDynamic_DynamicCRT
-cd build_x86_cimguiDynamic_DynamicCRT
+mkdir build_windows_x86_cimguiDynamic_DynamicCRT
+cd build_windows_x86_cimguiDynamic_DynamicCRT
 cmake -G "Visual Studio 17 2022" -AWin32 ..
 cmake --build . --config Release
 cd ..
-mkdir build_arm64_cimguiStatic_StaticCRT
-cd build_arm64_cimguiStatic_StaticCRT
+mkdir build_windows_arm64_cimguiStatic_StaticCRT
+cd build_windows_arm64_cimguiStatic_StaticCRT
 cmake -G "Visual Studio 17 2022" -AARM64 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT= ..
 cmake --build . --config Release
 cd ..
-mkdir build_arm64_cimguiStatic_DynamicCRT
-cd build_arm64_cimguiStatic_DynamicCRT
+mkdir build_windows_arm64_cimguiStatic_DynamicCRT
+cd build_windows_arm64_cimguiStatic_DynamicCRT
 cmake -G "Visual Studio 17 2022" -AARM64 -DSTATIC_CIMGUI= ..
 cmake --build . --config Release
 cd ..
-mkdir build_arm64_cimguiDynamic_StaticCRT
-cd build_arm64_cimguiDynamic_StaticCRT
+mkdir build_windows_arm64_cimguiDynamic_StaticCRT
+cd build_windows_arm64_cimguiDynamic_StaticCRT
 cmake -G "Visual Studio 17 2022" -AARM64 -DWINDOWS_STATIC_CRT= ..
 cmake --build . --config Release
 cd ..
-mkdir build_arm64_cimguiDynamic_DynamicCRT
-cd build_arm64_cimguiDynamic_DynamicCRT
+mkdir build_windows_arm64_cimguiDynamic_DynamicCRT
+cd build_windows_arm64_cimguiDynamic_DynamicCRT
 cmake -G "Visual Studio 17 2022" -AARM64 ..
 cmake --build . --config Release
 cd ..
