bdbcf506da18659dc4045ceea981791a6c80b138
Joshua T. Fisher <playmer@gmail.com>
2022-06-10 02:34:12 -0700

    Clean up BuildDependencies_Windows.bat

--- BuildDependencies_Windows.bat
+++ BuildDependencies_Windows.bat
@@ -1,62 +1,32 @@
-cd deps
-mkdir build_windows_x64_cimguiStatic_StaticCRT
-cd build_windows_x64_cimguiStatic_StaticCRT
-cmake -G "Visual Studio 17 2022" -Ax64 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_x64_cimguiStatic_DynamicCRT
-cd build_windows_x64_cimguiStatic_DynamicCRT
-cmake -G "Visual Studio 17 2022" -Ax64 -DSTATIC_CIMGUI= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_x64_cimguiDynamic_StaticCRT
-cd build_windows_x64_cimguiDynamic_StaticCRT
-cmake -G "Visual Studio 17 2022" -Ax64 -DWINDOWS_STATIC_CRT= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_x64_cimguiDynamic_DynamicCRT
-cd build_windows_x64_cimguiDynamic_DynamicCRT
-cmake -G "Visual Studio 17 2022" -Ax64 ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_x86_cimguiStatic_StaticCRT
-cd build_windows_x86_cimguiStatic_StaticCRT
-cmake -G "Visual Studio 17 2022" -AWin32 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_x86_cimguiStatic_DynamicCRT
-cd build_windows_x86_cimguiStatic_DynamicCRT
-cmake -G "Visual Studio 17 2022" -AWin32 -DSTATIC_CIMGUI= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_x86_cimguiDynamic_StaticCRT
-cd build_windows_x86_cimguiDynamic_StaticCRT
-cmake -G "Visual Studio 17 2022" -AWin32 -DWINDOWS_STATIC_CRT= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_x86_cimguiDynamic_DynamicCRT
-cd build_windows_x86_cimguiDynamic_DynamicCRT
-cmake -G "Visual Studio 17 2022" -AWin32 ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_arm64_cimguiStatic_StaticCRT
-cd build_windows_arm64_cimguiStatic_StaticCRT
-cmake -G "Visual Studio 17 2022" -AARM64 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_arm64_cimguiStatic_DynamicCRT
-cd build_windows_arm64_cimguiStatic_DynamicCRT
-cmake -G "Visual Studio 17 2022" -AARM64 -DSTATIC_CIMGUI= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_arm64_cimguiDynamic_StaticCRT
-cd build_windows_arm64_cimguiDynamic_StaticCRT
-cmake -G "Visual Studio 17 2022" -AARM64 -DWINDOWS_STATIC_CRT= ..
-cmake --build . --config Release
-cd ..
-mkdir build_windows_arm64_cimguiDynamic_DynamicCRT
-cd build_windows_arm64_cimguiDynamic_DynamicCRT
-cmake -G "Visual Studio 17 2022" -AARM64 ..
-cmake --build . --config Release
-cd ..
-cd ../../
+cmake -G "Visual Studio 17 2022" -Ax64 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT= -S deps -B deps/build_windows_x64_cimguiStatic_StaticCRT
+cmake --build deps/build_windows_x64_cimguiStatic_StaticCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -Ax64 -DSTATIC_CIMGUI= -S deps -B deps/build_windows_x64_cimguiStatic_DynamicCRT
+cmake --build deps/build_windows_x64_cimguiStatic_DynamicCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -Ax64 -DWINDOWS_STATIC_CRT= -S deps -B deps/build_windows_x64_cimguiDynamic_StaticCRT
+cmake --build deps/build_windows_x64_cimguiDynamic_StaticCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -Ax64 -S deps -B deps/build_windows_x64_cimguiDynamic_DynamicCRT
+cmake --build deps/build_windows_x64_cimguiDynamic_DynamicCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -AWin32 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT -S deps -B deps/build_windows_x86_cimguiStatic_StaticCRT
+cmake --build deps/build_windows_x86_cimguiStatic_StaticCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -AWin32 -DSTATIC_CIMGUI= -S deps -B deps/build_windows_x86_cimguiStatic_DynamicCRT
+cmake --build deps/build_windows_x86_cimguiStatic_DynamicCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -AWin32 -DWINDOWS_STATIC_CRT= -S deps -B deps/build_windows_x86_cimguiDynamic_StaticCRT
+cmake --build deps/build_windows_x86_cimguiDynamic_StaticCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -AWin32 -S deps -B deps/build_windows_x86_cimguiDynamic_DynamicCRT
+cmake --build deps/build_windows_x86_cimguiDynamic_DynamicCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -AARM64 -DSTATIC_CIMGUI= -DWINDOWS_STATIC_CRT -S deps -B deps/build_windows_arm64_cimguiStatic_StaticCRT
+cmake --build deps/build_windows_arm64_cimguiStatic_StaticCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -AARM64 -DSTATIC_CIMGUI= -S deps -B deps/build_windows_arm64_cimguiStatic_DynamicCRT
+cmake --build deps/build_windows_arm64_cimguiStatic_DynamicCRT --config Release
+
+cmake -G "Visual Studio 17 2022" -AARM64 -DWINDOWS_STATIC_CRT= -S deps -B deps/build_windows_arm64_cimguiDynamic_StaticCRT
+cmake --build deps/build_windows_arm64_cimguiDynamic_StaticCRT --config Release
